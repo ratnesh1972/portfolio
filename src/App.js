d0cc7f3b6a4238c08dc2fc8c66ef4491d657734f
Ratnesh Patil <ratnesh.designs@gmail.com>
2022-03-24 00:48:21 +0530

    Skills component added.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
+//Component Imports
+import Skills from './components/skills/Skills';
+
 function App() {
   return (
     <div className="App">
+      <Skills />
     </div>
   );
 }
---
+++ src/components/skills/Skills.js
@@ -0,0 +1,45 @@
+import skills from '../../assets/images/skills2.png';
+
+//Skills is a component which contains skills card and png.
+const Skills = () => {
+    return (
+        <section className="container mx-auto py-40">
+            {/* Heading and subheading of our section */}
+            <div className="prose lg:prose-xl text-center m-auto mb-5">
+                <h3 className="gradient-text">My Development Skills</h3>
+                <h1>The most creative intuitive workmate.</h1>
+            </div>
+            {/* Divide content and img into two columns */}
+            <div className="grid grid-cols-2 gap-5 mt-20">
+                {/* Content */}
+                <div className="prose items-center ">
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos cumque animi itaque perferendis commodi. Doloribus!</p>
+                    <div className="grid grid-cols-2 gap-5 mt-5 prose">
+                        <div className="bg-gray-100 px-4 rounded-2xl">
+                            <h4><span className="text-blue-600"><i class="fa-brands fa-react fa-lg" ></i></span> Frontend Development</h4>
+                            <p className="">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Omnis id ipsa eum neque minus dolorem numquam eius maxime doloribus vitae!</p>
+                        </div>
+                        <div className="bg-gray-100 px-4 rounded-2xl">
+                            <h4><span className="text-green-600"><i class="fa-brands fa-node fa-lg"></i></span> Backend Development</h4>
+                            <p className="">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Omnis id ipsa eum neque minus dolorem numquam eius maxime doloribus vitae!</p>
+                        </div>
+                        <div className="bg-gray-100 px-4 rounded-2xl">
+                            <h4><span className="text-rose-600"><i class="fa-solid fa-database fa-lg"></i></span> Database</h4>
+                            <p className="">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Omnis id ipsa eum neque minus dolorem numquam eius maxime doloribus vitae!</p>
+                        </div>
+                        <div className="bg-gray-100 px-4 rounded-2xl">
+                            <h4><span className="text-purple-600"><i class="fa-solid fa-pen-ruler fa-lg"></i></span> UI Design</h4>
+                            <p className="">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Omnis id ipsa eum neque minus dolorem numquam eius maxime doloribus vitae!</p>
+                        </div>
+                    </div>
+                </div>
+                {/* Image */}
+                <div className="pl-40 flex items-center">
+                    <img src={skills} alt="Skills" />
+                </div>
+            </div >
+        </section >
+    )
+}
+
+export default Skills;
