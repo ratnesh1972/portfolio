4d0f713d6053bb3fdbad21cfdc086246b2bd7b72
Ratnesh Patil <ratnesh.designs@gmail.com>
2022-03-25 23:52:18 +0530

    Navbar Added

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ import Projects from './components/projects/Projects';
 import Experience from './components/experience/Experience';
 import Connect from './components/connect/Connect';
 import Footer from './components/footer/Footer';
+import Navbar from './components/navbar/Navbar';
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <Skills />
       <Projects />
       <Experience />
---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,23 @@
+//Connect is a component which contains links for social sites and github.
+const Navbar = () => {
+    return (
+        <section className="sticky top-0 backdrop-saturate-50 backdrop-blur bg-slate-100/70">
+            <div className="container rounded-lg p-4 mx-auto">
+                <div className="flex flex-row gap-5 items-center justify-between">
+                    <h4 className="text-lg font-semibold">&#60; Ratnesh Patil /&#62;</h4>
+                    <div className="flex flex-row gap-8 text-md">
+                        <a href="#section1">Skills</a>
+                        <a href="#section2">Projects</a>
+                        <a href="#section3">Experience</a>
+                        <a href="#section4">Connect</a>
+                    </div>
+                    <div>
+                        <button><i class="fa-solid fa-moon fa-lg"></i></button>
+                    </div>
+                </div >
+            </div>
+        </section >
+    )
+}
+
+export default Navbar;
