7b1090e52a15dc6a0348b3bbcfcef7f91a581740
Ratnesh Patil <ratnesh.designs@gmail.com>
2022-03-28 13:06:06 +0530

    Made navbar responsive

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,18 +1,27 @@
+import React, { useState } from 'react';
+
 //Connect is a component which contains links for social sites and github.
 const Navbar = () => {
+    const [show, setshow] = useState(false);
+
+    const toggleShow = () => setshow(!show);
+
     return (
         <section className="fixed backdrop-saturate-50 backdrop-blur bg-slate-100/40 w-full">
             <div className="container rounded-lg p-4 mx-auto">
-                <div className="flex flex-row gap-5 items-center justify-between">
-                    <h4 className="text-lg font-semibold">&#60; Ratnesh Patil /&#62;</h4>
-                    <div className="flex flex-row gap-8 text-md">
+                <div className="grid grid-cols-2 gap-5 items-center justify-between md:grid-cols-3">
+                    <div className="order-1">
+                        <h4 className="text-lg font-semibold">&#60; Ratnesh Patil /&#62;</h4>
+                    </div>
+                    <div className={`order-3 ${show ? 'flex' : 'hidden'} ease-in-out flex-col gap-5 text-md md:justify-between md:flex-row md:gap-0 md:order-2 md:flex`}>
                         <a href="#section1">Skills</a>
                         <a href="#section2">Projects</a>
                         <a href="#section3">Experience</a>
                         <a href="#section4">Connect</a>
                     </div>
-                    <div>
-                        <button><i class="fa-solid fa-moon fa-lg"></i></button>
+                    <div className="justify-self-end order-2 md:order-3">
+                        {/* <button><i className="fa-solid fa-moon fa-lg"></i></button> */}
+                        <button className="block md:hidden" onClick={toggleShow}><i class="fa-solid fa-bars"></i></button>
                     </div>
                 </div >
             </div>
