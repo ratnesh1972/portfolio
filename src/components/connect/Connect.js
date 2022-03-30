7e719064a964ba7f985142661fa7d56aef1abbbb
Ratnesh Patil <ratnesh.designs@gmail.com>
2022-03-30 19:32:22 +0530

    Updated connect section links with rel attribute

--- src/components/connect/Connect.js
+++ src/components/connect/Connect.js
@@ -9,11 +9,11 @@ const Connect = () => {
             </div>
             {/* Divide content and img into 4 columns */}
             <div className="flex flex-col gap-10 mt-10 items-center justify-center lg:flex-row lg:mt-20">
-                <a class="rounded-lg bg-slate-900 shadow-lg shadow-slate-900/50 text-white text-center px-10 py-3 w-48" href="https://github.com/ratnesh1972" target="_blank"><i class="fa-brands fa-github fa-lg"></i> Github</a>
+                <a class="rounded-lg bg-slate-900 shadow-lg shadow-slate-900/50 text-white text-center px-10 py-3 w-48" href="https://github.com/ratnesh1972" target="_blank" rel="noopener"><i class="fa-brands fa-github fa-lg"></i> Github</a>
 
-                <a class="rounded-lg bg-blue-500 shadow-lg shadow-blue-500/50 text-white text-center px-10 py-3 w-48" href="https://www.linkedin.com/in/ratneshpatil/" target="_blank"><i class="fa-brands fa-linkedin fa-lg"></i> LinkedIn</a>
+                <a class="rounded-lg bg-blue-500 shadow-lg shadow-blue-500/50 text-white text-center px-10 py-3 w-48" href="https://www.linkedin.com/in/ratneshpatil/" target="_blank" rel="noopener><i class=" fa-brands fa-linkedin fa-lg"></i> LinkedIn</a>
 
-                <a class="rounded-lg bg-pink-500 shadow-lg shadow-pink-500/50 text-white text-center px-10 py-3 w-48" href="https://www.instagram.com/devmaniac173/" target="_blank"><i class="fa-brands fa-instagram fa-lg"></i> Instagram</a>
+                <a class="rounded-lg bg-pink-500 shadow-lg shadow-pink-500/50 text-white text-center px-10 py-3 w-48" href="https://www.instagram.com/devmaniac173/" target="_blank" rel="noopener><i class=" fa-brands fa-instagram fa-lg"></i> Instagram</a>
             </div >
         </section >
     )
